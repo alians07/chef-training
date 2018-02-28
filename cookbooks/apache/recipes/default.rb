4fd8487f6cafbc575ac58cc47ba7c3033d4ebe13
alians07 <alians07@yahoo.com>
2018-02-28 01:01:35 -0600

    Added MOTD.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,8 +5,16 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
 #
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
-	package_name 'httpd'
+
+	package_name package
 	action:install
 end
 
@@ -14,3 +22,6 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:enable,:start]
 end
+
+include_recipe 'apache::website'
+
